dc74452e136a69ed4a7c57351f211789efa88424
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2021-11-04 13:59:39 -0400

    Operator

---
+++ Project2_functions/main.js
@@ -0,0 +1,23 @@
+function myfav() {
+    var sentence = "My favourite";
+    sentence +=" color is BLUE!!";
+    document.getElementById("favo").innerHTML = sentence;
+}
+function myFuncation(){
+    var sentence = "Blue is";
+    sentence += " my favourite color!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+    var sentence = "Pink is";
+    sentence += " charm and sweet calmness!";
+    document.getElementById("Concatenate1").innerHTML = sentence;
+    var sentence = "Green is";
+    sentence += " cool, calm, inventive and logical!";
+    document.getElementById("Concatenate2").innerHTML = sentence;
+    var sentence = "Yellow is";
+    sentence += " an illuminating and cheerful essence!";
+    document.getElementById("Concatenate3").innerHTML = sentence;
+    var sentence = "Red is";
+    sentence += " not afraid of taking risks!";
+    document.getElementById("Concatenate4").innerHTML = sentence;
+}
+/* From this assignement I finish like insweting += operator and displaying funtion*/
