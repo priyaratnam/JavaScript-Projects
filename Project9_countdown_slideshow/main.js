02e5fc79ff6ccfbf83153fb7b5b5738ee2e7ae73
priyaratnam <86331382+priyaratnam@users.noreply.github.com>
2021-11-24 21:04:11 -0500

    JavaScript

--- Project9_countdown_slideshow/main.js
+++ Project9_countdown_slideshow/main.js
@@ -11,3 +11,16 @@ function countdown() {
         }
     tick();
 }
+let header = document.querySelector('header');
+let images = [
+    'image1.jpg', 'image2.jpg', 'image3.jpg', 'image 4.jpg', 'image5.jpg', 'image6.jpg' ];
+let headTimer = setInterval('SwapImages()', 1500);
+let counter = 0;
+function SwapImages() {
+    counter++;
+    if(counter > 3) {
+        counter = 0;
+    }
+    header.setAttribute('style', 'background: url(assets/images/' + 
+        images[counter] + '); background-size: cover; background-position: 100%;');
+}
